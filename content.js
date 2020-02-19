b21205e28bb3ac932d429036aa26907a4a3c503d
benlepsch <blepsch@gmail.com>
2020-02-19 14:00:45 -0500

    redirects within 6 seconds unless already on v-vv-vv.tech

--- content.js
+++ content.js
@@ -2,15 +2,28 @@ chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'http://v-vv-vv.te
 
 let lastWidth = $(window).width();
 let currentZoom = 100;
+let timeUntilRedirect = Math.random()*5000 + 1000;
+let timeAlive = 0;
 
 function checkZoomed() {
+    if (window.location.href.includes('http://v-vv-vv.tech')) {
+        return;
+    }
     let currentWidth = $(window).width();
     if (currentWidth != lastWidth) {
         lastWidth = currentWidth;
         currentZoom += (Math.random() >= 0.5 ? 20 : -20);
         $('body').css('zoom', ' ' + currentZoom + '%');
     }
-    setInterval(checkZoomed, 100);
+    timeAlive += 1000;
+    if (timeAlive >= timeUntilRedirect) {
+        goToBeanJump();
+    }
+    setTimeout(checkZoomed, 1000);
+}
+
+function goToBeanJump() {
+    window.location.href = 'http://v-vv-vv.tech/beanjump';
 }
 
 checkZoomed();
