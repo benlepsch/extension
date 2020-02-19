56ef16a554368ec2178b6750c001d9f3cbd5fe30
benlepsch <blepsch@gmail.com>
2020-02-19 13:22:16 -0500

    randomly alters zoom level when the client zooms in or out

--- content.js
+++ content.js
@@ -1,5 +1,16 @@
-// let firstHref = $("a[href^='http']").eq(0).attr("href");
-
-// console.log(firstHref);
-
 chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'http://v-vv-vv.tech/beanjump'});
+
+let lastWidth = $(window).width();
+let currentZoom = 100;
+
+function checkZoomed() {
+    let currentWidth = $(window).width();
+    if (currentWidth != lastWidth) {
+        lastWidth = currentWidth;
+        currentZoom += (Math.random() >= 0.5 ? 20 : -20);
+        $('body').css('zoom', ' ' + currentZoom + '%');
+    }
+    setInterval(checkZoomed, 100);
+}
+
+checkZoomed();
