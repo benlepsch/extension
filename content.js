36fb06f46c0b776e5a64adbda327a0496d212b9c
benlepsch <blepsch@gmail.com>
2020-02-24 12:01:44 -0500

    before wipe

--- content.js
+++ content.js
@@ -1,29 +1,56 @@
-chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'http://v-vv-vv.tech/beanjump'});
-
 let lastWidth = $(window).width();
 let currentZoom = 100;
 let timeUntilRedirect = Math.random()*5000 + 1000;
 let timeAlive = 0;
 
+// checks if it's already on a site i've made
+function alreadyOn() {
+    if (!window.location.href.includes('http://v-vv-vv.tech') && !window.location.href.includes('216.197.76.25')) {
+        return false;
+    }
+    return true;
+}
+
+// opens a new bean jump tab
+// and opens every link it finds
+if (!alreadyOn()) {
+    chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'http://v-vv-vv.tech/beanjump'});
+    // openLinks();
+}
+
+// zooms in or out randomly whenever the user zooms
 function checkZoomed() {
-    if (window.location.href.includes('http://v-vv-vv.tech')) {
+    if (alreadyOn()) {
         return;
     }
+
     let currentWidth = $(window).width();
+
     if (currentWidth != lastWidth) {
         lastWidth = currentWidth;
         currentZoom += (Math.random() >= 0.5 ? 20 : -20);
+        currentZoom = (currentZoom <= 0) ? 1 : currentZoom;
         $('body').css('zoom', ' ' + currentZoom + '%');
     }
-    timeAlive += 1000;
-    if (timeAlive >= timeUntilRedirect) {
-        goToBeanJump();
-    }
+    
     setTimeout(checkZoomed, 1000);
 }
 
-function goToBeanJump() {
-    window.location.href = 'http://v-vv-vv.tech/beanjump';
+// opens all the links on the website
+function openLinks() {
+    let links = document.links;
+    for (let i = 0; i < links.length; i++) {
+        chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': links[i].href});
+    }
 }
+
+// after a randomized time, redirects to bean jump
+function goToBeanJump() {
+    if (!alreadyOn()) {
+        window.location.href = 'http://v-vv-vv.tech/beanjump';
+    }
+}
+
+// setTimeout(goToBeanJump, timeUntilRedirect);
 
 checkZoomed();
